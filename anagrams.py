3a62cbfb9d95cfbeec6333cf2db3027cea4f35a3
Tiree Jackson <tiree_jackson@yahoo.com>
2020-09-26 19:52:04 -0400

    finish assessment

--- anagrams.py
+++ anagrams.py
@@ -10,7 +10,7 @@ for an arbitrary list of strings.
 
 # Your name here, and any other people/sources who helped.
 # Give credit where credit is due.
-__author__ = "???"
+__author__ = "Tiree help from Erica and John the Coach"
 
 import sys
 
@@ -27,11 +27,16 @@ def find_anagrams(words):
     Example:
     {'dgo': ['dog'], 'act': ['cat', 'act']}
     """
-    anagrams = {
-        alphabetize(word): [
-            w for w in words
-            if alphabetize(w) == alphabetize(word)]
-        for word in words}
+    anagrams = {}
+    for word in words:
+        anagrams.setdefault(alphabetize(word), [])
+        if word not in anagrams[alphabetize(word)]:
+            anagrams[alphabetize(word)].append(word)
+    # anagrams = {
+      #   alphabetize(word): [
+       #      w for w in words
+        #     if alphabetize(w) == alphabetize(word)]
+        # for word in words}
     return anagrams
 
 
